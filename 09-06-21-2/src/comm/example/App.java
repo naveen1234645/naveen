59d4235e074bb431cfef2fb509133647c41f3bce
001ZTG744 <001ZTG744@LAPTOP-55FBNAQE>
2021-06-09 14:55:49 +0530

    discount

--- 09-06-21-2/src/comm/example/App.java
+++ 09-06-21-2/src/comm/example/App.java
@@ -6,16 +6,16 @@ public class App {
 
 	public static void main(String[] args) {
 	Product product=new Product();
-	product.createProduct(UUID.randomUUID().toString(), "Cell Phone", 2000.0);
+	product.createProduct(UUID.randomUUID().toString(), "Cell Phone", 2000.0,2.0);
 	System.out.println(Product.getNumberOfProduct());
 	Product product1=new Product();
-	product1.createProduct(UUID.randomUUID().toString(), "Laptop", 2000.0);
+	product1.createProduct(UUID.randomUUID().toString(), "Laptop", 2000.0,2.5);
 	System.out.println(Product.getNumberOfProduct());
 	Product product2=new Product();
-	product2.createProduct(UUID.randomUUID().toString(), "Mobile", 2000.0);
+	product2.createProduct(UUID.randomUUID().toString(), "Mobile", 2000.0,2.8);
 	Product product3=new Product();
 	System.out.println(Product.getNumberOfProduct());
-	product3.createProduct(UUID.randomUUID().toString(), "Phone", 2000.0);
+	product3.createProduct(UUID.randomUUID().toString(), "Phone", 2000.0,2.3);
 	System.out.println(Product.getNumberOfProduct());
     
 
