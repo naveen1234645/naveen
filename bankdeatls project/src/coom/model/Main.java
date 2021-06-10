908d6bc7397beabb95cb19bb5d1e057722239485
001ZTG744 <001ZTG744@9.199.51.50>
2021-06-10 17:44:42 +0530

    Abstract Bank

---
+++ bankdeatls project/src/coom/model/Main.java
@@ -0,0 +1,26 @@
+package coom.model;
+import java.util.Scanner;
+public class Main{
+    
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int a2=sc.nextInt();
+        String b2=sc.nextLine();
+        sc.nextLine();
+        String c2=sc.nextLine();
+        Customer c=new Customer(a2,b2,c2);
+        System.out.println(" account balance");
+        double b=sc.nextDouble();
+        System.out.println(" enter min");
+        double d=sc.nextDouble();
+        System.out.println("Enter amount");
+        double f=sc.nextDouble();
+        SavingsAccount a1=new SavingsAccount(a,c,b,d);
+        boolean e=a1.withdraw(f);
+        if(e==true)
+        System.out.println("withdraw successful");
+        else
+        System.out.println("failed");
+    }
+}
