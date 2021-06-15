1d05205fe49c716e6c51b87c7e87714e497b01bc
001ZTG744 <001ZTG744@LAPTOP-55FBNAQE>
2021-06-15 06:49:46 +0530

    comparatorcommit

--- password/src/comm/model/Password.java
+++ password/src/comm/model/Password.java
@@ -1,61 +1,29 @@
 package comm.model;
 
+
+
 import java.util.Scanner;
 
 public class Password {
-    
-public static final int PASSWORD_LENGTH = 8;
+	private static Scanner sc = new Scanner(System.in);
 
-public static void main(String[] args) {
+	public static void main(String[] args) {
+		System.out.println("Enter password");
+		String password = sc.next();
+		boolean check = checkPassword(password);
+		if (check) {
+			System.out.println("Valid");
+		} else
+			System.out.println("Invalid");
 
-        Scanner input = new Scanner(System.in);
-        System.out.print(
-                "1. A password must have at least eight characters.\n" +
-                "2. A password consists of only letters and digits.\n" +
-                "3. A password must contain at least two digits \n" +
-                "Input a password (You are agreeing to the above Terms and Conditions.): ");
-        String s = input.nextLine();
+	}
 
-        if (is_Valid_Password(s)) {
-            System.out.println("Password is valid: " + s);
-        } else {
-            System.out.println("Not a valid password: " + s);
-        }
-
-    }
-
-    public static boolean is_Valid_Password(String password) {
-
-        if (password.length() < PASSWORD_LENGTH) return false;
-
-        int charCount = 0;
-        int numCount = 0;
-        for (int i = 0; i < password.length(); i++) {
-
-            char ch = password.charAt(i);
-
-            if (is_Numeric(ch)) numCount++;
-            else if (is_Letter(ch)) charCount++;
-            else return false;
-        }
-
-
-        return (charCount >= 2 && numCount >= 2);
-    }
-
-    public static boolean is_Letter(char ch) {
-        ch = Character.toUpperCase(ch);
-        return (ch >= 'A' && ch <= 'Z');
-    }
-    public static boolean is_Letter(char ch) {
-        ch = Character.toLowercaseCase(ch);
-        return (ch >= 'a' && ch <= 'z');
-    }
-
-
-    public static boolean is_Numeric(char ch) {
-
-        return (ch >= '0' && ch <= '9');
-    }
+	public static boolean checkPassword(String password) {
+		if (password.length() >= 8) {
+			String pattern = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}";
+			return password.matches(pattern);
+		}
+		return false;
+	}
 
 }
