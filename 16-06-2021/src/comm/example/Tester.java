6ea2d9e3d09dc5e0b06619728b0a237d60aa045b
001ZTG744 <001ZTG744@LAPTOP-55FBNAQE>
2021-06-16 17:48:06 +0530

    commit

--- 16-06-2021/src/comm/example/Tester.java
+++ 16-06-2021/src/comm/example/Tester.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
@@ -18,7 +19,9 @@ import comm.model.Todo;
 
 @SuppressWarnings("unused")
 public class Tester {
-
+private static Scanner scanner=new Scanner(System.in);
+	
+	
 
 	private Set<Employee> set = null;
 	private File file;
@@ -45,8 +48,107 @@ public class Tester {
 	}
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		Tester tester=new Tester();
+		
+		
+			int choice =0;
+			Tester tester=new Tester();
 
+
+			do {
+
+				System.out.println("Choose from the options below:");
+				System.out.println("1. Create:");
+				System.out.println("2. Remove:");
+				System.out.println("3. Update:");
+				System.out.println("4. Display:");
+				System.out.println("0. Exit:");
+
+				choice=scanner.nextInt();
+				switch(choice) {
+				case 1:
+					System.out.print("Enter the id and name: ");
+					String desc=scanner.next();
+					System.out.print("Enter the name: ");
+					int id=scanner.nextInt();
+					set.add(new Employee(UUID.randomUUID().toString(),id,name));
+					System.out.println("Created Sucessfully...");
+
+					break;
+				}
+
+
+	 	    case 2:
+	 	    	System.out.println("Enter Todo id: ");
+	 			String id = scanner.next();
+	 			Todo todo = null;
+	 			Iterator<Todo> i = set.iterator();
+	 			while(i.hasNext()) {
+	 				todo = i.next();
+	 				if(todo.getTodoId().equals(id)) {
+	 					System.out.println("found: " + todo);
+	 					set.remove(todo);
+	 					System.out.println("removed.");
+	 					break;
+	 				}
+	 				else
+	 				{
+	 					System.out.println("Invalid id");
+	 					break;
+	 				}
+	 			}
+
+
+				break;
+			
+
+
+		    case 3:
+		    	System.out.println("Enter Todo id: ");
+	 			String id1 = scanner.next();
+	 			Todo todo1 = null;
+	 			Iterator<Todo> j = set.iterator();
+	 			while(j.hasNext()) {
+	 				todo1 = j.next();
+	 				if(todo1.getTodoId().equals(id1)) {
+	 					System.out.println("found: " + todo1);
+	 					set.remove(todo1);
+	 				    System.out.print("Enter the description: ");
+	 					String desc1=scanner.next();
+	 					System.out.print("Enter the priority: ");
+	 					int priority1=scanner.nextInt();
+	 					set.add(new Todo(UUID.randomUUID().toString(),desc1,priority1));
+	 					System.out.println("Updated Sucessfully...");
+	 						break;
+	 				}
+	 				else
+	 				{
+	 					System.out.println("Invalid id");
+	 				}
+
+	 			}
+	 			break;
+
+			case 4:
+				for(Todo t:set)
+				{
+					System.out.println(t);
+				}
+
+				break;
+
+			case 0:
+
+				System.out.println("Exit from System. Bye..");
+				
+				System.exit(0);
+				break;
+				
+			default:
+				System.out.println("Invalid Choice Try Again.");
+				break;			
+
+			}	
+		}while(choice!=0);
 
 
 
