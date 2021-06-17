23c2737a2af14c67475670ddcf0f37b7eabb7d18
001ZTG744 <001ZTG744@9.85.92.58>
2021-06-17 12:36:05 +0530

    commit

--- Thread/src/comm/example/PrintThread.java
+++ Thread/src/comm/example/PrintThread.java
@@ -1,19 +1,26 @@
 package comm.example;
-
-public class PrintThread extends Thread {
-	public PrintThread(String name) {
+public class PrintThread extends Thread{
+	
+	public PrintThread(String name)
+	{
 		super(name);
 		start();
-		
+
 	}
+
 	@Override
 	public void run() {
 		String name=getName();
 
-		for(int i=0;i<1000;i++)
+		for(int i=0;i<100;i++)
 		{
 			System.out.println(name+"-->"+i);
-		}
+		//for(int i=0;i<100;i++)
+		//{
+			System.out.println(name);
 
-}
+		}
+		//}
+	}
+
 }
