f48eabdef99f739f284245eefa8b04926ca190c0
001ZTG744 <001ZTG744@9.77.192.217>
2021-06-22 09:28:11 +0530

    hibernate

---
+++ hibernate_demo-1/src/main/java/org/example/hibernate_demo_1/App.java
@@ -0,0 +1,13 @@
+package org.example.hibernate_demo_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
