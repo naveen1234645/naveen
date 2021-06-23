7077c8e98df22ef958286c8eb009f63fb42bed8e
001ZTG744 <001ZTG744@LAPTOP-55FBNAQE>
2021-06-23 13:25:59 +0530

    one-to-one-bi

--- hibernate-mapping-demo-1/src/main/java/org/example/demo/App.java
+++ hibernate-mapping-demo-1/src/main/java/org/example/demo/App.java
@@ -1,75 +1,71 @@
 package org.example.demo;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import javax.persistence.Query;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 import org.example.demo.model.Instructor;
 import org.example.demo.model.InstructorDetails;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
+public class App 
+{
+    @SuppressWarnings("unchecked")
+	public static void main( String[] args )
+    {
+    	Logger logger=Logger.getLogger("org.example.App.class");
+       try {
 
-public class App {
-	public static void main(String[] args) {
-		Logger logger = Logger.getLogger("org.example.demo.App.class");
-		try {
+    	   SessionFactory sessionFactory= new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetails.class).buildSessionFactory();
+    	   Session session=sessionFactory.getCurrentSession();
 
-			SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
-					.addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetails.class)
-					.buildSessionFactory();
-			Session session = factory.getCurrentSession();
-			/*
-			 * Instructor instructor=new Instructor(); InstructorDetails details=new
-			 * InstructorDetails(); details.setHobby("Singing");
-			 * details.setYoutubeChannel("channel-1"); instructor.setFirstName("John");
-			 * instructor.setLastName("Doe"); instructor.setEmail("john@email.com");
-			 * instructor.setInstructorDetails(details); session.getTransaction().begin();
-			 * session.persist(instructor); session.getTransaction().commit();
-			 */
+    	   Instructor instructor=new Instructor();
+    	   InstructorDetails details=new InstructorDetails();
+    	   instructor.setFirstName("ramu");
+    	   instructor.setLastName("raju");
+    	   instructor.setEmail("ramu@email.com");
+
+    	   details.setYoutubeChannel("my-channel-2");
+    	   details.setHobby("reading");
+    	   instructor.setInstructorDetails(details);
+    	   details.setInstructor(instructor);
     	   session.getTransaction().begin();
-    	   Instructor instructor=session.get(Instructor.class, 1);
-			/*
-			 * 
-			 * session.getTransaction().begin(); Instructor
-			 * instructor=session.get(Instructor.class, 1);
-			 * 
-			 * InstructorDetails details=instructor.getInstructorDetails();
-			 * details.setHobby("Cricket"); details.setYoutubeChannel("my-channel-1");
-			 * instructor.setEmail("john1@email.com"); session.merge(instructor);
-			 * session.getTransaction().commit();
-			 * 
-			 * 
-			 * session.delete(instructor); session.getTransaction().commit();
-			 * System.out.println("done");
-			 */
-
-    	   session.delete(instructor);
+    	   session.persist(instructor);
     	   session.getTransaction().commit();
-    	   System.out.println("done");
+    	   logger.info("Done!");
+//    
+    	   //For Display
+//    	   session=sessionFactory.openSession();
+//      List<Instructor> list=new ArrayList<Instructor>();
+//      Query query=session.createQuery("Select e from Instructor e");
+//      
+//      list=query.getResultList();
+//      Iterator<Instructor> i=list.iterator();
+//      while(i.hasNext())
+//    	  {
+//    	  System.out.println(i.next());
+//       }
 
-
-	} catch (Exception e) {
-		
-		e.printStackTrace();
-			InstructorDetails details = new InstructorDetails();
-			details.setHobby("Singing");
-			details.setYoutubeChannel("channel-1");
-			Instructor instructor = new Instructor();
-			instructor.setFirstName("John");
-			instructor.setLastName("doe");
-			instructor.setEmail("john@email.com");
-			details.setInstructor(instructor);
-			instructor.setInstructorDetails(details);
-			session.getTransaction().begin();
-			session.persist(details);
-			session.persist(instructor);
-			session.getTransaction().commit();
-			logger.info("done");
-
-		
-	}
+//    	   For Remove
+//    	   session.getTransaction().begin();
+//    	   
+//    	   session.remove(session.get(Instructor.class, 1));
+//    	   session.getTransaction().commit();
+//   		System.out.println("Employee Deleted Successfully!");
+       }catch(Exception e)
+       {
+    	   e.printStackTrace();
+       }
     }
 }
