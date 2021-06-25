e949fe2e2cf253bac092f647101b1526dd8a44fc
001ZTG744 <001ZTG744@LAPTOP-55FBNAQE>
2021-06-25 11:50:14 +0530

    added autowried

--- spring-annonation-ioc/src/main/java/org/example/spring_annonation_ioc/App.java
+++ spring-annonation-ioc/src/main/java/org/example/spring_annonation_ioc/App.java
@@ -8,28 +8,24 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Hello world!
  *
  */
-public class App 
-{
-	
-    public static void main( String[] args )
-    {
-    	try {
-    		@SuppressWarnings("resource")
-			ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
-    		Person person=context.getBean("thePerson",Person.class);
-    		person.setEmail("john@email.com");
-    		person.setFirstName("john");
-    		person.setLastName("doe");
-    		person.setPersonId(100);
-    		System.out.println(person);
-    		
-    		Person person1 = context.getBean("thePerson", Person.class);
-			System.out.println(person1);
-			System.out.println(person==person1);
-			
+public class App {
+
+	public static void main(String[] args) {
+		try {
+			@SuppressWarnings("resource")
+			ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+			Person person = context.getBean("person", Person.class);
+			System.out.println(person);
+
+			person.setEmail("john@email.com");
+			person.setFirstName("john");
+			person.setLastName("doe");
+			person.setPersonId(100);
+			System.out.println(person);
+
 		} catch (Exception e) {
 			e.printStackTrace();
-			
+
 		}
-    }
+	}
 }
