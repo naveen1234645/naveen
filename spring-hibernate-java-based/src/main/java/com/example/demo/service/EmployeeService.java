7e1abe674e3bfbcbd005db6fb46a4fb4c628283b
001ZTG744 <001ZTG744@9.199.54.154>
2021-06-29 15:19:40 +0530

    hibernet-java-based

---
+++ spring-hibernate-java-based/src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import com.example.demo.ui.EmployeeRequestModel;
+import com.example.demo.ui.EmployeeResponseModel;
+
+public interface EmployeeService {
+
+	public EmployeeResponseModel createEmployee(EmployeeRequestModel employeeDetails);
+
+}
