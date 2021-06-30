d6d63f79fa28464472d9df0f073b196a437f5dbf
naveen1234645 <85543563+naveen1234645@users.noreply.github.com>
2021-06-30 13:29:28 +0530

    Add files via upload

--- javascript-example/num.js
+++ javascript-example/num.js
@@ -14,7 +14,7 @@ else{
     console.log("Default fallback code");
 }*/
 
-for (var i = 1000; i <= 100; i += 2)
+/*for (var i = 1000; i <= 100; i += 2)
 {
     console.log("This is position for I: " + i);
 }
@@ -33,4 +33,90 @@ do
 {
     console.log("This is position for K: " + k);
     k += 2;
-} while (k <= 100)
+} while (k <= 100)*/
+
+
+/*let awesomeVar = 0;
+ 
+switch (awesomeVar)
+{
+    case 0:
+    console.log("This is 0");
+    break;
+ 
+    case 1:
+    console.log("This is 1");
+    break;
+ 
+    case 2:
+    console.log("This is 2");
+    break;
+ 
+    case 3:
+    console.log("This is 3");
+    break;
+ 
+    case 4:
+    console.log("This is 4");
+    break;
+ 
+    case 5:
+    console.log("This is 5");
+    break;
+ 
+    default:
+    console.log("This is the default value");
+    break;
+}*/
+/*function _myFunction()
+{
+    let a=100;
+    let b=200;
+    return('addition of a and b is: ',(a+b));
+}
+ 
+let result=_myFunction();
+console.log(result);
+ 
+function AwesomeFunc()
+{
+    console.log("Hello");
+    console.log("and");
+    console.log("Goodbye");
+}
+ 
+function Sum(num1, num2)
+{
+    let result = num1 + num2;
+ 
+    console.log(result);
+}
+ 
+function SumReturn(num1, num2)
+{
+    let result = num1 + num2;
+ 
+    return result;
+}
+ 
+AwesomeFunc();
+Sum(4, 7);
+console.log(SumReturn(5, 9));
+ 
+let epicVar = SumReturn(2, 100);
+ 
+console.log(epicVar);*/
+let var1 = 9;
+let var2 = 9;
+// Equal ===
+console.log(var1 === var2);
+// Not Equal !=
+console.log(var1 != var2);
+// Greater than >
+console.log(var1 > var2);
+// Less than <
+console.log(var1 < var2);
+// Greater than or equal to >=
+console.log(var1 >= var2);
+// Less than or equal to <=
+console.log(var1 <= var2);
