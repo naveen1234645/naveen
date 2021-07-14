0f379760712d473559dc4d9fa9383b4dd250ed3a
naveen1234645 <85543563+naveen1234645@users.noreply.github.com>
2021-07-14 12:01:34 +0530

    Add files via upload

--- microservice/order-service/src/main/java/com/example/demo/controller/OrderController.java
+++ microservice/order-service/src/main/java/com/example/demo/controller/OrderController.java
@@ -1,24 +1,44 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.OrderEntity;
+import com.example.demo.service.OrderService;
 
 @RestController
 public class OrderController {
 
-	
 	private Environment environment;
+	private OrderService orderService;
 
 	@Autowired
-	public OrderController(Environment environment) {
+	public OrderController(Environment environment,OrderService orderService) {
 		this.environment = environment;
+		this.orderService=orderService;
 	}
+
 	@GetMapping
-	public ResponseEntity<String> getStatus()
+	public ResponseEntity<String> getStatus() {
+		return ResponseEntity.ok("app running and up on port" + environment.getProperty("local.server.port"));
+	}
+	@PostMapping("/orders")
+	public ResponseEntity<OrderEntity> createOrder(@RequestBody OrderEntity orderEntity)
 	{
-		return ResponseEntity.ok("app running and up on port"+environment.getProperty("local.server.port"));
+		return new ResponseEntity<OrderEntity>(orderService.createOrder(orderEntity),HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/orders")
+	public ResponseEntity<List<OrderEntity>> displayOrders()
+	{
+		return ResponseEntity.ok(orderService.displayOrders());
 	}
 }
