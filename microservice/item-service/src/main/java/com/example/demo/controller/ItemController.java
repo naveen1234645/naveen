1c1af3d2ad5b2d1e1673a7eb6d09bf04b1490a28
naveen1234645 <85543563+naveen1234645@users.noreply.github.com>
2021-07-15 06:38:22 +0530

    Add files via upload

--- microservice/item-service/src/main/java/com/example/demo/controller/ItemController.java
+++ microservice/item-service/src/main/java/com/example/demo/controller/ItemController.java
@@ -1,13 +1,4 @@
 package com.example.demo.controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,31 +9,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.dto.ItemDto;
+import com.example.demo.model.Order;
 import com.example.demo.service.ItemService;
 import com.example.demo.ui.ItemRequestModel;
 import com.example.demo.ui.ItemResponseModel;
 @RestController
-@RequestMapping("/items")
-
 public class ItemController {
+	
 	private ItemService itemService;
 	private ModelMapper modelMapper;
 	private Environment environment;
+	private RestTemplate restTemplate;
 	@Autowired
 	public ItemController(ItemService itemService, ModelMapper modelMapper,Environment environment) {
 		this.itemService = itemService;
 		this.modelMapper = modelMapper;
 		this.environment=environment;
+		this.restTemplate=restTemplate;
 	}
-	@GetMapping("/status")
+	@GetMapping
 	public ResponseEntity<String> getStatuc()
 	{
 		return ResponseEntity.ok("application running port:"+environment.getProperty("local.server.port"));
 	}
 	
-	@PostMapping
+	@PostMapping("/items")
 	public ResponseEntity<ItemResponseModel> createItem(@RequestBody ItemRequestModel itemDetails)
 	{
 		System.out.println(itemDetails);
@@ -54,34 +54,30 @@ public class ItemController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(model);
 		
 	}
-	@GetMapping
+	@GetMapping("/items")
 	public ResponseEntity<List<ItemResponseModel>> getAllItems()
 	{
 		return ResponseEntity.status(HttpStatus.OK).body(itemService.getAllItems());
-		
 	}
-	@GetMapping("/find/{itemName}")
+
+	@GetMapping("/items/find/{itemName}")
 	public ResponseEntity<List<ItemResponseModel>> getByItemname(@PathVariable("itemName") String itemName)
 	{
 		return ResponseEntity.ok(itemService.findByItemName(itemName));
 	}
-	@GetMapping("/{itemNumber}")
+	@GetMapping("/items/{itemNumber}")
 	public ResponseEntity<ItemResponseModel> getByItemNumber(@PathVariable("itemNumber") String itemNumber)
 	{
 		return ResponseEntity.ok(itemService.findByItemNumber(itemNumber));
 	}
-	@PutMapping("/{itemNumber}")
-	public ResponseEntity<ItemResponseModel> updateItem(@PathVariable("itemNumber") String itemNumber,@RequestBody() ItemResponseModel itemDetails)
+	@GetMapping("/items/orders")
+	public ResponseEntity<List<Order>> getOrderFromOrderService()
 	{
-		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-		ItemDto dto=modelMapper.map(itemDetails, ItemDto.class);
-		ItemResponseModel model=itemService.updateItem(itemNumber,dto);
-		return ResponseEntity.status(HttpStatus.OK).body(model);
+		String uri="http://localhost:8088/order-service/orders";
+		@SuppressWarnings("unchecked")
+		List<Order> orders=restTemplate.getForObject(uri, List.class);
+		return ResponseEntity.ok(orders);
 	}
-	@DeleteMapping("/{itemNumber}")
-	public ResponseEntity<String> deleteItem(@PathVariable("itemNumber") String itemNumber)
-	{
-		return ResponseEntity.ok(itemService.deleteItem(itemNumber));
-	}
-
+	
+	
 }
