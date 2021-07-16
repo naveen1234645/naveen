2ea756e3278a2c0bd34cbc6ecbc375a66653ab9d
naveen1234645 <85543563+naveen1234645@users.noreply.github.com>
2021-07-16 16:42:16 +0530

    Add files via upload

--- microservice/issue-service/src/main/java/com/example/demo/controller/IssueController.java
+++ microservice/issue-service/src/main/java/com/example/demo/controller/IssueController.java
@@ -1,9 +1,10 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,65 +14,124 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.Assignee;
 import com.example.demo.model.Issue;
+import com.example.demo.model.Severity;
+import com.example.demo.proxy.AssigneeClient;
 import com.example.demo.service.IssueService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class IssueController {
-
 	private IssueService issueService;
 	private Environment environment;
+	private AssigneeClient assigneeClient;
 
 	@Autowired
-	public IssueController(IssueService issueService, Environment environment) {
+	public IssueController(IssueService issueService, Environment environment, AssigneeClient assigneeClient) {
+		
 		this.issueService = issueService;
 		this.environment = environment;
+		this.assigneeClient = assigneeClient;
 	}
 
 	@GetMapping
-	public ResponseEntity<String> getStatus() {
-		return new ResponseEntity<String>(
-				"issue service is up and running on port: " + environment.getProperty("local.server.port"),
-				HttpStatus.OK);
+	public ResponseEntity<String> getStatus()
+	{
+		return new ResponseEntity<String>("issue service is up and running on port: "+environment.getProperty("local.server.port"),HttpStatus.OK);
 	}
-
+	
 	@PostMapping("/issues")
-	public ResponseEntity<Issue> createIssue(@RequestBody Issue issue) {
-		return new ResponseEntity<Issue>(issueService.createNewIssue(issue), HttpStatus.CREATED);
+	public ResponseEntity<Issue> createIssue(@RequestBody Issue issue) throws Exception
+	{
+		Assignee assignee=assigneeClient.getAssigneeByAssigneeName(issue.getAssignee());
+		if(assignee.getIsAvailable()==false)
+		{
+			throw new Exception();
+		}
+		return new ResponseEntity<Issue>(issueService.createIssue(issue),HttpStatus.CREATED);
 	}
-
+	
 	@GetMapping("/issues")
-	public ResponseEntity<List<Issue>> displayIssues() {
-		return new ResponseEntity<List<Issue>>(issueService.displayAllIssues(), HttpStatus.OK);
+	public ResponseEntity<List<Issue>> getAllIssues()
+	{
+		return new ResponseEntity<List<Issue>>(issueService.getAllIssues(),HttpStatus.OK);
 	}
+	
+	@GetMapping("/issues/assignees")
+	@HystrixCommand(fallbackMethod = "assigneeFallBack")
+	public ResponseEntity<List<Assignee>> getAllAssignee()
+	{
+		List<Assignee> list=new ArrayList<Assignee>();
+		list=assigneeClient.getAllAssignee();
+		System.out.println(list);
+		return ResponseEntity.ok(list);
+	}
+	
+	public ResponseEntity<List<Assignee>> assigneeFallBack()
+	{
+		List<Assignee> list=new ArrayList<Assignee>();
+		list.add(0,null);
+		return ResponseEntity.ok(list);
 
+	}
+	
 	@GetMapping("/issues/{id}")
-	public ResponseEntity<Optional<Issue>> getIssueById(@PathVariable("id") String id) {
-		return ResponseEntity.status(HttpStatus.OK).body(issueService.findIssueById(id));
+	public ResponseEntity<Issue> findIssueById(@PathVariable("id") String id)
+	{
+		return new ResponseEntity<Issue>(issueService.findIssueById(id),HttpStatus.OK);
 	}
-	
-	@GetMapping("/issues/{description}")
-	public ResponseEntity<Optional<Issue>> getIssueByDescription(@PathVariable("description") String description) {
-		return ResponseEntity.status(HttpStatus.OK).body(issueService.findIssueByDescription(description));
-	}
-	
-	
 	@PutMapping("/issues/{id}")
-	public ResponseEntity<Issue> updateIssue(@PathVariable("id") String id, @RequestBody Issue issue) {
-		return ResponseEntity.ok(issueService.updateIssue(id, issue));
+	public ResponseEntity<Issue> updateIssue(@PathVariable("id") String id,@RequestBody Issue issue)
+	{
+		return new ResponseEntity<Issue>(issueService.updateIssue(id, issue),HttpStatus.OK);
 	}
-
-
-	/*@PutMapping("/issues/{description}")
-	public ResponseEntity<Issue> updateIssue(@PathVariable("description") String description, @RequestBody Issue issue) {
-		return ResponseEntity.ok(issueService.updateIssue(description, issue));
-	}*/
 	
 	@DeleteMapping("/issues/{id}")
-	public ResponseEntity<Issue> deleteIssue(@PathVariable("id") String id) {
-		return ResponseEntity.ok(issueService.removeIssue(id));
+	public ResponseEntity<String> deleteIssue(@PathVariable("id") String id)
+	{
+		return new ResponseEntity<String>(issueService.deleteIssue(id),HttpStatus.OK);
 	}
 
+	@GetMapping("/issues/find/{assignee}")
+	public ResponseEntity<Issue> findIssueByAssignee(@PathVariable("assignee") String assignee)
+	{
+		return new ResponseEntity<Issue>(issueService.findIssueByAssignee(assignee),HttpStatus.OK);
+	}
+	@GetMapping("/findsevere/{severity}")
+	public ResponseEntity<List<Issue>> findIssueByAssignee(@PathVariable("severity") Severity severity)
+	{
+		return new ResponseEntity<List<Issue>>(issueService.findIssueBySeverity(severity),HttpStatus.OK);
+	}
+	@GetMapping("/issues/finddesc/{desc}")
+	public ResponseEntity<Issue> findIssueByDescription(@PathVariable("desc") String desc)
+	{
+		return new ResponseEntity<Issue>(issueService.findIssueByDescription(desc),HttpStatus.OK);
+	}
+	@PutMapping("/issues/assignee/{assignee}")
+	public ResponseEntity<Issue> updateIssueByAssignee(@PathVariable("assignee") String assignee,@RequestBody Issue issue)
+	{
+		return new ResponseEntity<Issue>(issueService.updateIssueByAssignee(assignee, issue),HttpStatus.OK);
+	}
+	@PutMapping("/issues/desc/{desc}")
+	public ResponseEntity<Issue> updateIssueByDescription(@PathVariable("desc") String desc,@RequestBody Issue issue)
+	{
+		return new ResponseEntity<Issue>(issueService.updateIssueByDescription(desc, issue),HttpStatus.OK);
+	}
+	@DeleteMapping("/issues/assignee/{assignee}")
+	public ResponseEntity<String> deleteIssueByAssignee(@PathVariable("assignee") String assignee)
+	{
+		return new ResponseEntity<String>(issueService.deleteIssueByAssignee(assignee),HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/issues/desc/{desc}")
+	public ResponseEntity<String> deleteIssueByDescription(@PathVariable("desc") String desc)
+	{
+		return new ResponseEntity<String>(issueService.deleteIssueByDescription(desc),HttpStatus.OK);
+	}
+	
+	
 }
