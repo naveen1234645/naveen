7da36b106a1ff62be1f0348b367acdc23360665c
naveen1234645 <85543563+naveen1234645@users.noreply.github.com>
2021-07-18 14:41:19 +0530

    Add files via upload

---
+++ microservice/employee-service/src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.Employee;
+
+public interface EmployeeService {
+
+	
+	public Employee createEmployee(Employee employee);
+	
+	public List<Employee> displayEmployee();
+}
